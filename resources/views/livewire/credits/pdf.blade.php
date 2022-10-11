75c09e553eeef838bdd371c62f7c3414af6a782f
Henry Gustavo X. Az <azhenrygus@outlook.es>
2022-10-11 00:16:08 -0600

    conflict ammend resolved

--- resources/views/livewire/credits/pdf.blade.php
+++ resources/views/livewire/credits/pdf.blade.php
@@ -3,111 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< Updated upstream
-  {{-- <link rel="stylesheet" href="{{asset('resources/view/pdf.css')}}"> --}}
-=======
-  <link rel="stylesheet" href="{{public_path('css/pdf.css')}}" type="text/css">
->>>>>>> Stashed changes
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">  <link rel="stylesheet" href="{{public_path('css/pdf.css')}}" type="text/css">
+
+  
   <title>estimacion</title>
 </head>
 <body>
   
-<<<<<<< Updated upstream
-  <style>
-    @page { size: 21.59cm 27.94cm; }
-    body {
-      font-family: Arial, Helvetica, sans-serif;
-    }
-
-    h1,h2,h3,h4,h5,h6 {
-      font-weight: 300;
-    }
-
-    table {
-      max-width: 500px;
-      width: 100%;
-      border-collapse: collapse;
-    }
-
-    table thead tr th {
-      text-align: left;
-      font-weight: 300;
-    }
-
-    table thead tr {
-      background: #ccc; 
-    }
-
-    table thead tr th, table tbody tr td{
-      padding: .8em;
-    }
-  </style>
-  <h1>pdf</h1>
-
-  {{-- @for($i=0; $i<count($interests); $i++) 
-    <p>{{$interests[$i]}}</p>
-  @endfor
- --}}
-
-  <table >
-    <thead >
-        <tr class="">
-            <th scope="col">
-              Pago No.
-            </th>
-            <th scope="col" >
-                fecha pago
-            </th>
-            <th scope="col">
-                cuota fija
-            </th>
-            <th scope="col" >
-                interes
-            </th>
-            <th scope="col" >
-                capital
-            </th>
-            <th scope="col">
-              saldo
-            </th>
-        </tr>
-    </thead>
-    <tbody >
-      @for($i=0;$i<count($balances)  ;$i++)
-        <tr >
-
-          <td scope="row" >
-            {{$paymentNumber[$i] ?? 1}}
-          </td>
-
-            <td scope="row" >
-              {{$dates[$i]}}
-              
-            </td>
-            <td scope="row" >
-              Q. {{$fees[$i]}}
-            </td>
-            <td scope="row" >
-              Q. {{$paymentInterests[$i]}}
-            </td>
-            <td scope="row" >
-              Q. {{$currentCapital[$i]}}
-            </td>
-            <td scope="row" >
-              Q. {{$balances[$i]}}
-            </td>
-            
-
-        </tr>
-        @endfor
-
-
-        
-    </tbody>
-  </table>
-
-=======
   <div class="container" style="page-break-after: auto">
   <h1 class="title">Proyección</h1>
 
@@ -176,10 +78,10 @@
         <tr class="table__body-row table__row">
           <td class="table__body-column table__column">{{$paymentNumber[$i]}}</td>
           <td class="table__body-column table__column">{{$dates[$i]}}</td>
-          <td class="table__body-column table__column">{{$fees[$i]}}</td>
-          <td class="table__body-column table__column">{{$paymentInterests[$i]}}</td>
-          <td class="table__body-column table__column">{{$currentCapital[$i]}}</td>
-          <td class="table__body-column table__column">{{$balances[$i]}}</td>
+          <td class="table__body-column table__column">Q.{{$fees[$i]}}</td>
+          <td class="table__body-column table__column">Q.{{$paymentInterests[$i]}}</td>
+          <td class="table__body-column table__column">Q.{{$currentCapital[$i]}}</td>
+          <td class="table__body-column table__column">Q.{{$balances[$i]}}</td>
         </tr>
       @endfor
       
@@ -187,6 +89,6 @@
   </table>
 
 </div>
->>>>>>> Stashed changes
+
 </body>
 </html>
