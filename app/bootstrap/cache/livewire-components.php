aa0b194740b381e002bbabc804a45df75f01ed5a
HenryXAz <82301988+HenryXAz@users.noreply.github.com>
2022-10-28 01:39:22 -0600

    Create livewire-components.php

---
+++ app/bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'customer-reports' => 'App\\Http\\Livewire\\CustomerReports',
+  'customers' => 'App\\Http\\Livewire\\Customers',
+  'new-credits' => 'App\\Http\\Livewire\\NewCredits',
+  'profits' => 'App\\Http\\Livewire\\Profits',
+  'show-balances' => 'App\\Http\\Livewire\\ShowBalances',
+  'users' => 'App\\Http\\Livewire\\Users',
+);
