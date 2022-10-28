431ff47be3af5d94c981d4617c398774da7cdea2
HenryXAz <82301988+HenryXAz@users.noreply.github.com>
2022-10-28 00:07:03 -0600

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,7 +21,8 @@
     </style>
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+ <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased text-gray-900 dark:text-gray-200">
