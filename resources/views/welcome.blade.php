664afe9e3dbd8aa83336914c17b9e63371063ff8
HenryXAz <82301988+HenryXAz@users.noreply.github.com>
2022-10-28 00:50:45 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,8 @@
         />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css'])
+        <link rel="stylesheet" href="{{ asset('build/assets/app.7f022113.css ') }}">
+        <script src="{{ asset('build/assets/app.5b2bdc69.js ') }}" defer></script>
     </head>
     <body class="antialiased font-sans bg-gray-100 text-gray-900 dark:bg-dark-eval-0 dark:text-gray-200">
         <div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
