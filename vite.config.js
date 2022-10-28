7a4ca166f858aaa5ec5a40ab374f105e3bbec922
HenryXAz <82301988+HenryXAz@users.noreply.github.com>
2022-10-27 23:38:19 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,16 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-        }),
+        },
+         server: {
+    proxy: {
+    '/api': {
+      target: 'https://app-ventas-al-credito-production.up.railway.app/',
+      changeOrigin: true,
+      secure: false,
+    },
+    cors:false
+    }, 
+       ),
     ],
 });
