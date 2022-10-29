0b5d2746456640a8c327b0cecc686aa4f70daf2f
Henry Gustavo Xitimul Az <azhenrygus@outlook.es>
2022-10-29 14:33:32 -0600

    bootstrap js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,7 +10,7 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
