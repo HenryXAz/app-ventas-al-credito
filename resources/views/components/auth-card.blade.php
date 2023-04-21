f8f188d24c4f4f408cc48a6aa92c5113d3bcc462
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-04-20 23:40:38 -0600

    logo size edited

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -7,7 +7,7 @@
         {{-- <h2 class="text-3xl text-purple-600 dark:text-purple-500">Iniciar Sesión</h2> --}}
 
         <a href="/">
-          <img src="{{asset('images/logoBussiness.jpg')}}" alt="logo" width="200">
+          <img src="{{asset('images/logo-business.png')}}" alt="logo" width="100">
         </a>
 
     </div>
