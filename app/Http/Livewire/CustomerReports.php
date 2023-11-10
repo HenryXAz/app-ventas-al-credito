7fbc7f6dfc63a1bbae6d0080b14dee04e9650c10
Willie Gramajo <gramajo.zw@gmail.com>
2023-11-09 20:51:59 -0600

    cambios al reporte clientes fecha inicio y fecha fin

--- app/Http/Livewire/CustomerReports.php
+++ app/Http/Livewire/CustomerReports.php
@@ -4,67 +4,111 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Credit;
-use Livewire\WithPagination;
 use Carbon\Carbon;
+use Barryvdh\DomPDF\Facade as PDF;
+use Illuminate\Support\Facades\Log;
 
 class CustomerReports extends Component
 {
-    use WithPagination;
+    public $startDate, $endDate, $paidCredits, $unpaidCredits, $upcomingPayments;
 
-    public $report = "1";
-
-
-    public function updatingReport()
+    public function mount()
     {
-      $this->resetPage();
+        // Establece las fechas iniciales en formato Y-m-d
+        $this->startDate = Carbon::today('America/Guatemala')->subDays(7)->format('Y-m-d');
+        $this->endDate = Carbon::today('America/Guatemala')->format('Y-m-d');
+        $this->refreshData();
     }
 
+    public function updateDateRange()
+    {
+        // Asegúrate de que las fechas estén en formato Y-m-d
+        $this->validate([
+            'startDate' => 'required|date_format:Y-m-d',
+            'endDate' => 'required|date_format:Y-m-d',
+        ]);
 
-    public function render()
-    {   
-        $date = Carbon::today("America/Guatemala");
-        
-        $startWeek = $date->startOfWeek()->toDateString();
-        $endWeek = $date->endOfWeek()->toDateString();
-        
+        $this->refreshData();
+    }
 
-        $credits = [];
+    private function queryCredits($type)
+    {
+        $query = Credit::query();
+        $startDateInstance = Carbon::createFromFormat('Y-m-d', $this->startDate)->startOfDay();
+        $endDateInstance = Carbon::createFromFormat('Y-m-d', $this->endDate)->endOfDay();
 
-        if($this->report === "1") {
-          $credits = Credit::where("status", "=", "1")
-          ->whereIn("id", function($query) use($startWeek, $endWeek)
-          {
-            $query->select("id_credit")
-              ->from("payments")
-              ->where("payments.status", "=", "1")
-              ->whereBetween("payment_date", [$startWeek, $endWeek]);
-          })->paginate(10);
-        } else if($this->report === "2") {
-          $credits = Credit::where("status", "=", "1")
-            ->whereNotIn("id", function($query) use($startWeek, $endWeek)
-          {
-            $query->select("id_credit")
-              ->from("payments")
-              ->where("payments.status", "=", "1")
-              ->whereBetween("payment_date", [$startWeek, $endWeek]);
-          })->paginate(10);
+        switch ($type) {
+            case 'paid':
+                $query->where('status', '1')->whereHas('payments', function ($query) use ($startDateInstance, $endDateInstance) {
+                    $query->where('status', '2')->whereBetween('payment_date', [$startDateInstance, $endDateInstance]);
+                });
+                break;
 
+            case 'unpaid':
+                $query->where('status', '1')->whereDoesntHave('payments', function ($query) use ($startDateInstance, $endDateInstance) {
+                    $query->where('status', '1')->whereBetween('payment_date', [$startDateInstance, $endDateInstance]);
+                });
+                break;
 
-        } else if($this->report === "3") {
-          $credits = Credit::where("status", "=", "1")
-            ->whereIn("id", function($query) use($startWeek, $endWeek)
-          {
-            $query->select("id_credit")
-              ->from("payments")
-              ->where("status", "=", "1")
-              ->where("payment_date", "<", Carbon::today("America/Guatemala"));
-          })->paginate(10);
+            case 'upcoming':
+                $query->where('status', '1')->with(['futurePayments' => function ($query) use ($endDateInstance) {
+                    $query->where('payment_date', '>', $endDateInstance);
+                }])->whereHas('futurePayments', function ($query) use ($endDateInstance) {
+                    $query->where('payment_date', '>', $endDateInstance);
+                });
+                break;
         }
 
-
-        return view('livewire.customer-reports.customer-reports', [
-          "credits" => $credits,
-        ]);        
+        Log::info("Query SQL for {$type}: " . $query->toSql());
+        return $query->get();
     }
 
+    public function refreshData()
+    {
+        $this->paidCredits = $this->queryCredits('paid');
+        $this->unpaidCredits = $this->queryCredits('unpaid');
+        $this->upcomingPayments = $this->queryCredits('upcoming');
+    }
+
+    public function render()
+    {
+        // Convertir las fechas a d/m/Y para mostrarlas en la vista
+        $startDateDisplay = Carbon::createFromFormat('Y-m-d', $this->startDate)->format('d/m/Y');
+        $endDateDisplay = Carbon::createFromFormat('Y-m-d', $this->endDate)->format('d/m/Y');
+
+        return view('livewire.customer-reports.customer-reports', [
+            'startDate' => $startDateDisplay,
+            'endDate' => $endDateDisplay,
+            'paidCredits' => $this->paidCredits,
+            'unpaidCredits' => $this->unpaidCredits,
+            'upcomingPayments' => $this->upcomingPayments,
+        ]);
+    }
+
+    public function exportToPDF()
+    {
+        // La validación para la exportación debe ser en el formato d/m/Y porque es el formato que se mostrará en el PDF
+        $this->validate([
+            'startDate' => 'required|date_format:d/m/Y',
+            'endDate' => 'required|date_format:d/m/Y',
+        ]);
+
+        $data = [
+            'startDate' => $this->startDate,
+            'endDate' => $this->endDate,
+            'paidCredits' => $this->paidCredits,
+            'unpaidCredits' => $this->unpaidCredits,
+            'upcomingPayments' => $this->upcomingPayments,
+        ];
+
+        $pdf = PDF::loadView('livewire.customer-reports.pdf-customers', $data);
+
+        $startDateForFilename = Carbon::createFromFormat('d/m/Y', $this->startDate)->format('Y-m-d');
+        $endDateForFilename = Carbon::createFromFormat('d/m/Y', $this->endDate)->format('Y-m-d');
+
+        return response()->streamDownload(
+            fn () => print($pdf->output()),
+            "customer-report-{$startDateForFilename}-to-{$endDateForFilename}.pdf"
+        );
+    }
 }
--- resources/views/livewire/customer-reports/customer-reports.blade.php
+++ resources/views/livewire/customer-reports/customer-reports.blade.php
@@ -1,95 +1,149 @@
-<div class="w-full">
+<div>
+    <style>
+        .date-selector input[type='date'] {
+            border: 1px solid #ced4da;
+            padding: .375rem .75rem;
+            border-radius: .25rem;
+            color: #495057;
+            background-color: #fff;
+            transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        }
+        .date-selector input[type='date']:focus {
+            border-color: #80bdff;
+            outline: 0;
+            box-shadow: 0 0 0 .2rem rgba(13, 90, 167, 0.25);
+        }
+        .table {
+            width: 100%;
+            margin-bottom: 1rem;
+            color: #1b76d1;
+        }
+        .table th,
+        .table td {
+            padding: .75rem;
+            vertical-align: top;
+            border-top: 1px solid #dee2e6;
+        }
+        .table thead th {
+            vertical-align: bottom;
+            border-bottom: 2px solid #dee2e6;
+        }
+        .table tbody + tbody {
+            border-top: 2px solid #dee2e6;
+        }
+        .table .table {
+            background-color: #fff;
+        }
+        .loading {
+            text-align: center;
+            padding: 1rem;
+        }
+    </style>
 
-    <h1 class="w-full text-center font-light my-5 text-3xl">Reporte de Clientes</h1>
-
-
-    <div class="flex gap-4 items-center my-6">
-        <div class="flex flex-col gap-2 ">
-            ¿Qué desea ver?
-            <select name="" id="report" class="dark:bg-dark-eval-2 dark:text-white text-gray-700 bg=white" wire:model="report">
-                <option value="1">pagan esta semana</option>
-                <option value="2">no pagan esta semana</option>
-                <option value="3">pagos atrasados</option>
-            </select>
-        </div>
+<div class="date-selector" x-data="{ startDate: @entangle('startDate'), endDate: @entangle('endDate') }">
+    <input type="text" x-ref="start" x-model="startDate" placeholder="dd/mm/YYYY">
+    <input type="text" x-ref="end" x-model="endDate" placeholder="dd/mm/YYYY">
+    <button wire:click="updateDateRange">Actualizar</button>
+    <button wire:click="exportToPDF" wire:loading.attr="disabled">Exportar a PDF</button>
+</div>
+    <div class="loading" wire:loading>
+        Cargando los datos...
     </div>
 
-   
-    @if(count($credits) > 0)
-    <form action="{{route('pdfCustomers')}}" method="POST" target="_blank">
-      @csrf
-      <input type="hidden" name="report" id="" value="{{json_encode($report)}}" >
-      <button type="submit" wire:submit.prevent="submit" class="px-2 py-4 rounded-md text-white bg-indigo-600 hover:bg-indigo-700 ">generar reporte</button>
-    </form>
+    <h2>Créditos Pagados</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Monto</th>
+                <th>Fecha de Pago</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($paidCredits as $credit)
+            <tr>
+                <td>{{ $credit->customer->name ?? 'N/A' }}</td>
+                <td>{{ number_format($credit->fee ?? 0, 2, ',', '.') }}</td>
+                <td>
+                    @php
+                        $payment = $credit->payments->where('status', '2')->first();
+                    @endphp
+                    {{ $payment ? \Carbon\Carbon::parse($payment->payment_date)->format('d/m/Y') : 'N/A' }}
+                </td>
+            </tr>
+        @empty
+                <tr><td colspan="3">No hay créditos pagados en este rango de fechas.</td></tr>
+            @endforelse
+        </tbody>
+    </table>
 
-    <div class="overflow-x-auto relative ">
-      <table class="w-full mt-5 text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="bg-dark-eval-3 text-xs uppercase  dark:bg-dark-eval-1 text-white ">
-              <tr class="">
+    <h2>Créditos No Pagados</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Monto</th>
+                <th>Fecha de Vencimiento</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($unpaidCredits as $credit)
+                <tr>
+                    <td>{{ $credit->customer->name ?? 'N/A' }}</td>
+                    <td>{{ number_format($credit->fee ?? 0, 2, ',', '.') }}</td>
+                    <td>{{ \Carbon\Carbon::parse($credit->due_date)->format('d/m/Y') }}</td>
+                </tr>
+            @empty
+                <tr><td colspan="3">No hay créditos no pagados en este rango de fechas.</td></tr>
+            @endforelse
+        </tbody>
+    </table>
 
-                  <th scope="col" class="py-3 px-6">
-                      nombre
-                  </th>
-                  <th scope="col" class="py-3 px-6">
-                      foto de perfil
-                  </th>
-                  <th scope="col" class="py-3 px-6">
-                      email
-                  </th>
+    <h2>Próximos Pagos</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Monto Próximo Pago</th>
+                <th>Fecha Próximo Pago</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($upcomingPayments as $payment)
+            <tr>
+                <td>{{ $payment->customer->name ?? 'N/A' }}</td>
+                <td>{{ number_format($payment->fee ?? 0, 2, ',', '.') }}</td>
+                <td>{{ \Carbon\Carbon::parse($payment->payment_date)->format('d/m/Y') }}</td>
+            </tr>
+        @empty
+            <tr><td colspan="3">No hay pagos próximos en este rango de fechas.</td></tr>
+        @endforelse
+        </tbody>
+    </table>
+    @push('scripts')
+    <script>
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('datePicker', () => ({
+                init() {
+                    this.$nextTick(() => {
+                        flatpickr(this.$refs.start, {
+                            dateFormat: 'd/m/Y',
+                            onChange: (selectedDates, dateStr, instance) => {
+                                this.startDate = dateStr;
+                            },
+                        });
+                        flatpickr(this.$refs.end, {
+                            dateFormat: 'd/m/Y',
+                            onChange: (selectedDates, dateStr, instance) => {
+                                this.endDate = dateStr;
+                            },
+                        });
+                    });
+                },
+            }));
 
-                  <th scope="col" class="py-3 px-6">
-                      teléfono
-                  </th>
-  
-                  <th scope="col" class="py-3 px-6">
-                      monto
-                  </th>
-                  <th scope="col" class="py-3 px-6">
-                      fecha pago
-                  </th>
-  
-              </tr>
-          </thead>
-          <tbody>
-              @foreach($credits as $credit)
-              <tr class="bg-white dark:bg-dark-eval-2 even:bg-purple-100 dark:even:bg-dark-eval-1 ">
-                  <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap  dark:text-white">
-                    {{$credit->customer->name}} {{$credit->customer->last_name}}
-                  </td>
-
-                  <td class="py-4 px-6 text-gray-900 dark:text-white ">
-                    <img src="{{asset('storage/' . $credit->customer->photo)}}" alt="customer photo" width="200">
-                  </td>
-                  <td class="py-4 px-6 text-gray-900  dark:text-white">
-                      {{$credit->customer->email}}
-                  </td>
-                  <td class="py-4 px-6 text-gray-900  dark:text-white">
-                      {{$credit->customer->personal_phone}}
-                  </td>
-        
-                  <td class="py-4 px-6 text-gray-900  dark:text-white">
-                      Q. {{$credit->nextPayment($credit->id)->fee ?? 0}}
-                  </td>
-                  <td class="py-4 px-6 text-gray-900  dark:text-white">
-                      {{\Carbon\Carbon::parse($credit->nextPayment($credit->id)->payment_date)->format('d-m-Y') ?? 0}}
-                  </td>
-
-              </tr>
-              @endforeach
-          </tbody>
-      </table>
-  </div>
-
-  <div class="px-6 py-3 w-1/2 mx-auto">
-    {{$credits->links()}}
-  </div>
-    @else 
-      <div class="w-full mx-auto my-6 ">
-        <p class="text-gray-700 dark:text-white text-center font-light text-xl">
-          Nada para mostrar
-        </p>
-      </div>
-
-    @endif
-
+            Alpine.start();
+        });
+    </script>
+    @endpush
 </div>
