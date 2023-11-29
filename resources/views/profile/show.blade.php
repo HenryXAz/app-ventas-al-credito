feaa3c7da498d69d4303c71a227d9ff74766bd67
Henry Gustavo X Az <azhenrygus@outlook.es>
2023-11-28 23:12:47 -0600

    add ui for profile configuration

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,4 +1,23 @@
 <x-app-layout>
-    <h1>Saldo Clientes</h1>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight">
+            {{ __('Profile Settings') }}
+        </h2>
+    </x-slot>
 
+    <div class="grid gap-8">
+        @if (Laravel\Fortify\Features::canUpdateProfileInformation())
+            @livewire('profile.update-profile-information-form')
+        @endif
+
+        @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
+            @livewire('profile.update-password-form')
+        @endif
+
+        @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
+            @livewire('profile.two-factor-authentication-form')
+        @endif
+
+        @livewire('profile.logout-other-browser-sessions-form')
+    </div>
 </x-app-layout>
